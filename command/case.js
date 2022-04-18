b7a75fbe7c8c0e7c0cd3c79e28c11c07c22fd72f
Juan Argote <89502105+Argote-314@users.noreply.github.com>
2022-04-18 05:40:53 -0500

    Update case.js

--- command/case.js
+++ command/case.js
@@ -404,60 +404,61 @@ case 'owner':{
 					}, 'contactsArrayMessage', { quoted: mek })
 					haruka.sendMessage(from, `Nih Kak Contact Owner Ku, Cuma Sv Nomor Cewe Ya 🤝`, text, {quoted: hehe})
 				}
-			break
+	break
+		
 case 'sticker':case 'stiker':case 'stickergif':case 'stikergif':case 'sgif':case 's':
 if (!isHaruka) return sendButMessage(from, lang.noregis(pushname), `Klik Button Untuk Verify`, [{buttonId: '.daftar',buttonText: {displayText: `Daftar`,},type: 1,}], {quoted: fgif});
-			if ((isMedia && !mek.message.videoMessage || isQuotedImage) && args.length == 0) {
-			const encmedia = isQuotedImage ? JSON.parse(JSON.stringify(mek).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : mek
-			const media = await haruka.downloadAndSaveMediaMessage(encmedia)
-			ran = getRandom('.webp')
-			await ffmpeg(`./${media}`)
-			.input(media)
-			.on('start', function (cmd) {
-				console.log(`Started : ${cmd}`)
-				})
-				.on('error', function (err) {
-					console.log(`Error : ${err}`)
-					fs.unlinkSync(media)
-					reply('Eror')
-					})
-			.on('end', function () {
-				console.log('Finish')
-				haruka.sendMessage(from, fs.readFileSync(ran), sticker, { quoted: mek })
-				fs.unlinkSync(media)
-				fs.unlinkSync(ran)
-				})
-				.addOutputOptions([`-vcodec`, `libwebp`, `-vf`, `scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
-				.toFormat('webp')
-				.save(ran)
-				} else if ((isMedia && mek.message.videoMessage.seconds < 11 || isQuotedVideo && mek.message.extendedTextMessage.contextInfo.quotedMessage.videoMessage.seconds < 11) && args.length == 0) {
-				const encmedia = isQuotedVideo ? JSON.parse(JSON.stringify(mek).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : mek
-				const media = await haruka.downloadAndSaveMediaMessage(encmedia)
-				ran = getRandom('.webp')
-				await ffmpeg(`./${media}`)
-				.inputFormat(media.split('.')[1])
-				.on('start', function (cmd) {
-					console.log(`Started : ${cmd}`)
-					})
-					.on('error', function (err) {
-						console.log(`Error : ${err}`)
-						fs.unlinkSync(media)
-						tipe = media.endsWith('.mp4') ? 'video' : 'gif'
-						reply(`❌ Gagal, pada saat mengkonversi ${tipe} ke stiker`)
-						})
-						.on('end', function () {
-							console.log('Finish')
-							haruka.sendMessage(from, fs.readFileSync(ran), sticker, { quoted: mek })
-							fs.unlinkSync(media)
-							fs.unlinkSync(ran)
-							})
-							.addOutputOptions([`-vcodec`, `libwebp`, `-vf`, `scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
-							.toFormat('webp')
-							.save(ran)
-							} else  {
-								reply(`Kirim gambar dengan caption ${prefix}sticker atau tag gambar yang sudah dikirim\nDurasi sticker video 1-9 detik...`)
-							}
-					
+if ((isMedia && !mek.message.videoMessage || isQuotedImage) && args.length == 0) {
+const encmedialoli = isQuotedImage ? JSON.parse(JSON.stringify(mek).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : mek
+const media = await haruka.downloadAndSaveMediaMessage(encmedialoli)
+ran = '666.webp'
+await ffmpeg(`./${media}`)
+.input(media)
+.on('start', function (cmd) {
+console.log(`Started : ${cmd}`)
+})
+.on('error', function (err) {
+console.log(`Error : ${err}`)
+fs.unlinkSync(media)
+reply('error')
+})
+.on('end', function () {
+console.log('Finish')
+haruka.sendMessage(from, fs.readFileSync(ran), sticker, {quoted: mek})
+fs.unlinkSync(media)
+fs.unlinkSync(ran)
+})
+.addOutputOptions([`-vcodec`, `libwebp`, `-vf`, `scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
+.toFormat('webp')
+.save(ran)
+} else if ((isMedia && mek.message.videoMessage.seconds < 11 || isQuotedVideo && mek.message.extendedTextMessage.contextInfo.quotedMessage.videoMessage.seconds < 11) && args.length == 0) {
+const encmedialoli22 = isQuotedVideo ? JSON.parse(JSON.stringify(mek).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : mek
+const media = await haruka.downloadAndSaveMediaMessage(encmedialoli22)
+ran = '999.webp'
+reply(mess.wait)
+await ffmpeg(`./${media}`)
+.inputFormat(media.split('.')[1])
+.on('start', function (cmd) {
+console.log(`Started : ${cmd}`)
+})
+.on('error', function (err) {
+console.log(`Error : ${err}`)
+fs.unlinkSync(media)
+tipe = media.endsWith('.mp4') ? 'video' : 'gif'
+reply(`Gagal, pada saat mengkonversi ${tipe} ke stiker`)
+})
+.on('end', function () {
+console.log('Finish')
+haruka.sendMessage(from, fs.readFileSync(ran), sticker, {quoted: mek})
+fs.unlinkSync(media)
+fs.unlinkSync(ran)
+})
+.addOutputOptions([`-vcodec`, `libwebp`, `-vf`, `scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
+.toFormat('webp')
+.save(ran)
+} else {
+reply(`Send a picture with a caption ${prefix}sticker\nVideo Sticker Duration 1-9 Seconds`)
+}					
              break
 					
 // download fix by zeeone
